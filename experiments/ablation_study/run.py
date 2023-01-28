b350bea1b943e74054facf328187bfab718d34cf
ian ubuntu <iabr4073@gmail.com>
2023-01-28 17:21:13 -0500

    got good parameters

--- experiments/ablation_study/run.py
+++ experiments/ablation_study/run.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from build_solver import build_erg_time_opt_solver
 
 
-erg_ubs = [0.001, 0.01, 0.1]
+erg_ubs = [0.001, 0.005, 0.01, 0.05, 0.1]
 for i, erg_ub in enumerate(erg_ubs):
     args = {
         'N' : 200, 
@@ -14,7 +14,7 @@ for i, erg_ub in enumerate(erg_ubs):
         # 'alpha' : 0.8,
     }
     solver = build_erg_time_opt_solver(args)
-    solver.solve(max_iter=10000, eps=1e-6)
+    solver.solve(max_iter=10000, eps=1e-7)
     sol = solver.get_solution()
     print(sol['tf'], erg_ub)
     plt.figure(i)
