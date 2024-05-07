66d9e413cff4c04a5e415b064686e5cdfea6e5d5
Ethan Dong <dayiethan@gmail.com>
2024-05-07 13:33:19 -0400

    knot study additional results but multiscale is not working properly

--- experiments/Journal/knot_study/build_solver.py
+++ experiments/Journal/knot_study/build_solver.py
@@ -107,13 +107,92 @@ def build_erg_time_opt_solver(init_sol, args, num_basis=8, step_size=1e-3, c=1.0
     return solver
 
 
-def build_erg_time_opt_solver_print(init_sol, args, step_size=1e-3, c=1.0):
-    basis           = BasisFunc(n_basis=[8,8])
+def build_erg_time_opt_solver_print(init_sol, args, num_basis=8, step_size=1e-3, c=1.0):
+    basis           = BasisFunc(n_basis=[num_basis]*2)
     erg_metric      = ErgodicMetric(basis)
     robot_model     = DoubleIntegrator()
     n,m = robot_model.n, robot_model.m
     target_distr    = TargetDistribution()
     workspace_bnds = [[0.,1.0],[0.,1.0]]
+   
+    args.update({
+        'phik' : get_phik(target_distr.evals, basis),
+    })
+
+    @vmap
+    def emap(x):
+        """ Function that maps states to workspace """
+        return np.array([
+            (x[0]-workspace_bnds[0][0])/(workspace_bnds[0][1]-workspace_bnds[0][0]), 
+            (x[1]-workspace_bnds[1][0])/(workspace_bnds[1][1]-workspace_bnds[1][0])])
+            
+    def barrier_cost(e):
+        """ Barrier function to avoid robot going out of workspace """
+        return (np.maximum(0, e-1) + np.maximum(0, -e))**2
+
+    # @jit
+    def loss(params, args):
+        x = params['x']
+        u = params['u']
+        tf = params['tf']
+        N = args['N']
+        dt = tf/N
+        e = emap(x)
+        """ Traj opt loss function, not the same as erg metric """
+        return np.sum(barrier_cost(e)) + tf
+
+    def eq_constr(params, args):
+        """ dynamic equality constriants """
+        x = params['x']
+        u = params['u']
+
+        x0 = args['x0']
+        xf = args['xf']
+        tf = params['tf']
+        N = args['N']
+        dt = tf/N
+        return np.vstack([
+            x[0] - x0, 
+            x[1:,:]-(x[:-1,:]+dt*vmap(robot_model.dfdt)(x[:-1,:], u[:-1,:])),
+            x[-1] - xf
+        ])
+
+    def ineq_constr(params, args):
+        """ inequality constraints"""
+        x = params['x']
+        u = params['u']
+        phik = args['phik']
+        tf = params['tf']
+        N = args['N']
+        dt = tf/N
+        e = emap(x)
+
+        ck = get_ck(e, basis, tf, dt)
+        erg = erg_metric(ck, phik)
+        deb.print("erg: {a}", a=erg)
+        _erg_ineq = [np.array([erg_metric(ck, phik) - args['erg_ub'], -tf])]
+        _ctrl_box = [(np.abs(u) - 1.).flatten()]
+        _expl_box = [(-e).flatten(), (e-1.0).flatten()]
+        return np.concatenate(_erg_ineq + _ctrl_box + _expl_box)
+
+    solver = AugmentedLagrangeSolver(
+                    init_sol,
+                    loss, 
+                    eq_constr, 
+                    ineq_constr, 
+                    args, 
+                    step_size=step_size,
+                    c=c)
+    return solver
+    
+
+def build_erg_time_opt_solver_wkspace(init_sol, args, num_basis=8, step_size=1e-3, c=1.0):
+    basis           = BasisFunc(n_basis=[num_basis]*2)
+    erg_metric      = ErgodicMetric(basis)
+    robot_model     = DoubleIntegrator()
+    n,m = robot_model.n, robot_model.m
+    target_distr    = TargetDistribution()
+    workspace_bnds = args['wrksp_bnds']
    
     args.update({
         'phik' : get_phik(target_distr.evals, basis),
@@ -184,4 +263,83 @@ def build_erg_time_opt_solver_print(init_sol, args, step_size=1e-3, c=1.0):
                     step_size=step_size,
                     c=c)
     return solver
-    
+
+
+def build_erg_time_opt_solver_scale(args):
+    basis           = BasisFunc(n_basis=[10,10])
+    erg_metric      = ErgodicMetric(basis)
+    robot_model     = DoubleIntegrator()
+    n,m = robot_model.n, robot_model.m
+    target_distr    = TargetDistribution()
+
+    args.update({
+        'phik' : get_phik(target_distr.evals, basis),
+    })
+
+    ## <--- I DO NOT LIKE THIS
+    workspace_bnds = args['wrksp_bnds']
+
+    @vmap
+    def emap(x):
+        """ Function that maps states to workspace """
+        return np.array([
+            (x[0]-workspace_bnds[0][0])/(workspace_bnds[0][1]-workspace_bnds[0][0]), 
+            (x[1]-workspace_bnds[1][0])/(workspace_bnds[1][1]-workspace_bnds[1][0])])
+            
+    def barrier_cost(e):
+        """ Barrier function to avoid robot going out of workspace """
+        return (np.maximum(0, e-1) + np.maximum(0, -e))**2
+
+    # @jit
+    def loss(params, args):
+        x = params['x']
+        u = params['u']
+        tf = params['tf']
+        N = args['N']
+        dt = tf/N
+        e = emap(x)
+        """ Traj opt loss function, not the same as erg metric """
+        return np.sum(barrier_cost(e)) + tf
+
+    def eq_constr(params, args):
+        """ dynamic equality constriants """
+        x = params['x']
+        u = params['u']
+
+        x0 = args['x0']
+        xf = args['xf']
+        tf = params['tf']
+        N = args['N']
+        dt = tf/N
+        return np.vstack([
+            x[0] - x0, 
+            x[1:,:]-(x[:-1,:]+dt*vmap(robot_model.dfdt)(x[:-1,:], u[:-1,:])),
+            x[-1] - xf
+        ])
+
+    def ineq_constr(params, args):
+        """ inequality constraints"""
+        x = params['x']
+        u = params['u']
+        phik = args['phik']
+        tf = params['tf']
+        N = args['N']
+        dt = tf/N
+        e = emap(x)
+        ck = get_ck(e, basis, tf, dt)
+        _erg_ineq = [np.array([erg_metric(ck, phik) - args['erg_ub'], -tf])]
+        _ctrl_box = [(np.abs(u) - 20).flatten()]
+        return np.concatenate(_erg_ineq + _ctrl_box)# + _obs_constr)
+
+    x = np.linspace(args['x0'], args['xf'], args['N'], endpoint=True)
+    u = np.zeros((args['N'], robot_model.m))
+    init_sol = {'x': x, 'u' : u, 'tf': np.array(50.0)}
+    solver = AugmentedLagrangeSolver(
+                    init_sol,
+                    loss, 
+                    eq_constr, 
+                    ineq_constr, 
+                    args, 
+                    step_size=1e-2,
+                    c=1.)
+    return solver
